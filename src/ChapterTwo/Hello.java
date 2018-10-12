c146f321d1ec2afceb6a06dc9d30c2055ebfe105
jiangfeng <1546844168@qq.com>
2018-10-12 16:33:29 +0800

    创建了一个hello类用与测试

---
+++ src/ChapterTwo/Hello.java
@@ -0,0 +1,11 @@
+package ChapterTwo;
+
+/**
+ * @author 江峰
+ * @create 2018-10-12 16:32
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
