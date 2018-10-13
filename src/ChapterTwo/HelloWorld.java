da9603088950ac88589ca03ec6539e7f88ee5b4f
jiangfeng <1546844168@qq.com>
2018-10-13 14:05:59 +0800

    学习effective java 第二章

---
+++ src/ChapterTwo/HelloWorld.java
@@ -0,0 +1,12 @@
+package ChapterTwo;
+
+/**
+ * @author 江峰
+ * @create 2018-10-13 14:00
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("helloworld");
+    }
+
+}
