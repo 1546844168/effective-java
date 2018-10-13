5e62bfdff3240595070c1479e3e53389a2bf7e56
jiangfeng <1546844168@qq.com>
2018-10-13 11:50:13 +0800

    修改了three.java

--- src/ChapterTwo/three.java
+++ src/ChapterTwo/three.java
@@ -1,11 +1,7 @@
 package ChapterTwo;
 
-/**
- * @author 江峰
- * @create 2018-10-13 11:01
- */
-public class three {
+class Three{
     public static void main(String[] args){
-        System.out.println("local");
+        System.out.println("three");
     }
 }
