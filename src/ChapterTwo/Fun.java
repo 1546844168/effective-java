d52a82991e8971e3aea3b64dd8b841ff8f4732be
jiangfeng <1546844168@qq.com>
2018-10-13 10:11:43 +0800

    新文件

---
+++ src/ChapterTwo/Fun.java
@@ -0,0 +1,11 @@
+package ChapterTwo;
+
+/**
+ * @author 江峰
+ * @create 2018-10-13 10:10
+ */
+public class Fun {
+    public static void main(String[] args){
+        System.out.println("开心");
+    }
+}
