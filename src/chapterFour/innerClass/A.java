37ab121df219d09a0ffab5117891b1d0c6650c87
feng.jiang <feng.jiang@irootech.com>
2018-10-19 16:39:37 +0800

    学习effective java

---
+++ src/chapterFour/innerClass/A.java
@@ -0,0 +1,38 @@
+package chapterFour.innerClass;
+
+import java.util.HashMap;
+import java.util.Set;
+
+/**
+ * @author 江峰
+ * @create 2018-10-16 12:34
+ */
+class A {
+    private B b = new B();
+
+    public void fun() {
+        System.out.println("out");
+        b.inner();
+    }
+
+    public B returnB() {
+        B b = new B();
+        return b;
+    }
+
+    class B {
+        public void inner() {
+            fun(); // 非静态成员类内部方法可以直接调用外围内的方法。
+            System.out.println("inner");
+        }
+    }
+
+    public static void main(String[] args) {
+//      new A().new B().inner();
+        new A().fun(); //
+
+        HashMap hashMap = new HashMap();
+        final Set set = hashMap.keySet();
+    }
+
+}
