beaf7570159f03d6e02e674783466d223fbb9111
jiangfeng <1546844168@qq.com>
2018-10-21 23:05:50 +0800

    学习笔记

---
+++ src/chapterEight/Main.java
@@ -0,0 +1,13 @@
+package chapterEight;
+
+/**
+ * @author 潇潇暮雨
+ * @create 2018-10-21   10:43
+ */
+public class Main {
+    public static void main(String[] args){
+
+    }
+
+}
+
--- src/chapterSeven/ReturnNull.java
+++ src/chapterSeven/ReturnNull.java
@@ -15,9 +15,11 @@ public class ReturnNull {
     }
 
     public static int[] fun(int[] arr) {
-        if (arr.length == 0) {
+        // 写出健壮性的代码，需要对入参的参数进行判断，养成一个好习惯。
+        if (arr == null)
+            return null;
+        if (arr.length == 0)
             return arr;
-        }
         return arr;
     }
 }
